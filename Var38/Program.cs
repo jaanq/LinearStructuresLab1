1a5ef680082f0377c91a2094409ec9fbe2d138b0
Ruslan <ruslan.semack1501@gmail.com>
2020-10-18 23:16:07 +0300

    init

---
+++ Var38/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Var38
+{           // Массив, Стек, Быстрая сортировка Хоару (c медианным (pivot) элемнтом)
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack(5);
+            stack.Push(1);
+            stack.Pop();
+            Console.WriteLine(stack.Peek());
+        }
+    }
+}
---
+++ Var38/Stack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Var38
+{
+    class Stack
+    {
+        int[] storage;
+        int length;
+        int maxlength;
+        public Stack(int n)
+        {
+            storage = new int[n];
+            maxlength = n;
+            length = n-1;
+        }
+
+        public void Push(int input)
+        {
+            storage[length] = input;
+            if (length != 0)
+            {
+                length--;
+            }
+        }
+        public int Pop()
+        {
+            int toReturn = storage[length];
+            //Array.Clear(storage, length,maxlength);
+            storage[length] = 0;
+            if (length != maxlength - 1)
+            {
+                length++;
+            }
+            return toReturn;
+        }
+        public int Peek()
+        {
+            return storage[length];
+        }
+        public int Length() { return maxlength; }
+
+        public void Sort() { }
+    }
+}
